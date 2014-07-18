f646fa6b2407f6e2376acf09954ed5881bcea0c2
Robert Kent <robert@wearetheweather.co.uk>
2014-07-18 12:23:08 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1, user-scalable=0"/>
+        <title>Snake</title>
+        <link rel="stylesheet" href="../css/bootstrap.min.css"/>
+        <link href='http://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body class="container">
+        <div class="canvas-container">
+            <p>Use the arrow keys to control the snake</p>
+
+            <canvas id="jsSnake"></canvas>
+
+            <div id="controls">
+                <button class="control" id="up">Up</button>
+                <button class="control" id="left">Left</button>
+                <button class="control" id="right">Right</button>
+                <button class="control" id="down">Down</button>
+            </div>
+
+            <div id="game-over">
+                <h3>Game Over</h3>
+
+                <div id="score-form">
+                    <p>Enter your name to save your score</p>
+
+                    <p>
+                        <input type="text" name="name" id="name"/>
+                    </p>
+
+                    <p>
+                        <button id="submit-score">Submit</button>
+                    </p>
+                </div>
+                <p>Press space or tap the game to restart</p>
+            </div>
+        </div>
+
+        <?php
+
+        $host = 'localhost';
+        $dbname = 'robert_test';
+        $user = 'snake_game';
+        $pass = 'bS4JqAApZCmjdCGP';
+
+        try {
+            $dbh = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $pass);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+        $sth = $dbh->query('SELECT * FROM snake_scores ORDER BY score DESC, created DESC LIMIT 5');
+        $sth->setFetchMode(PDO::FETCH_ASSOC);
+        ?>
+        <div id="scores">
+            <h2>High Scores</h2>
+            <table class="table" id="scores">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = $sth->fetch()) { ?>
+                        <tr>
+                            <td><?php echo $row['name'] ?></td>
+                            <td><?php echo $row['score'] ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js"></script>
+        <script src="buzz.js"></script>
+        <script src="game.js"></script>
+        <script>
+            $(window).on('load', function (){
+                JS_SNAKE.game.init();
+            });
+        </script>
+    </body>
+</html>
---
+++ process-score.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robert
+ * Date: 14/07/2014
+ * Time: 10:22 AM
+ *
+ * Gets a POST of a username and a score to enter into the snake leaderboard.
+ * Enters the score into the database then returns the HTML for displaying in the table
+ */
+
+// Get POST variables
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$score = filter_input(INPUT_POST, 'score', FILTER_SANITIZE_NUMBER_INT);
+$date = date('Y-m-d H:i:s');
+
+// Enter score into database
+$host = 'localhost';
+$dbname = 'robert_test';
+$user = 'snake_game';
+$pass = 'bS4JqAApZCmjdCGP';
+
+try {
+    $dbh = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $pass);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+$data = array(
+    'name' => $name,
+    'score' => $score,
+    'created' => $date
+);
+
+$sth = $dbh->prepare('INSERT INTO snake_scores (name, score, created) VALUE (:name, :score, :created)');
+$sth->execute($data);
+
+// Get top scores and return HTML
+$sth = $dbh->query('SELECT * FROM snake_scores ORDER BY score DESC, created DESC LIMIT 5');
+$sth->setFetchMode(PDO::FETCH_ASSOC);
+
+$result = '';
+
+while ($row = $sth->fetch()) {
+    $result .= "<tr>
+                    <td>{$row['name']}</td>
+                    <td>{$row['score']}</td>
+                </tr>";
+}
+
+echo $result;
